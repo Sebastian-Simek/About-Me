ce20f342b0010cc2ef93014481f54c81b06a0f50
Sebastian-Simek <sebastiansimek09@gmail.com>
2022-04-20 15:08:57 -0700

    Added functionality to button click

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
-// import functions
 
-// grab DOM elements
+const showButton = document.getElementById('button');
+const Animal = document.getElementById('animalDiv');
 
-// set event listeners 
-    // get user input
-    // use user input to update state 
-    // update DOM to reflect the new state
+showButton.addEventListener('click', () => {
+    Animal.classList.remove('hidden');
+});
