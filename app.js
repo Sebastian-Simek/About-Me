b96621b23b4722ff7f6e1f47592db9ea3e27c959
Sebastian-Simek <sebastiansimek09@gmail.com>
2022-04-20 16:27:28 -0700

    Added feature to remove button after click

--- app.js
+++ app.js
@@ -5,16 +5,16 @@ const animal = document.getElementById('animalDiv');
 const nameBtn = document.getElementById('nameButton');
 const myName = document.getElementById('name');
 
-showButton.addEventListener('click', () => {
-    animal.classList.remove('hidden');
-});
 
 nameBtn.addEventListener('click', () => {
     if (myName.textContent === '') { 
         myName.textContent = 'Sebastian';
     } else {
-        myName.textContent = '';
+        myName.textContent = '____';
     }
-}
-
-);
+});
+showButton.classList.remove('hide');
+showButton.addEventListener('click', () => {
+    animal.classList.remove('hidden');
+    showButton.classList.add('hide');
+});
