0ee235115bcddfcf91d0dff482052ec16c6a8e3a
Sebastian-Simek <sebastiansimek09@gmail.com>
2022-04-21 14:50:17 -0700

    switched to .toggle instead of .remove

--- app.js
+++ app.js
@@ -1,20 +1,17 @@
 
 const showButton = document.getElementById('button');
 const animal = document.getElementById('animalDiv');
-
 const nameBtn = document.getElementById('nameButton');
 const myName = document.getElementById('name');
 
-
 nameBtn.addEventListener('click', () => {
-    if (myName.textContent === '') { 
+    if (myName.textContent === '____') { 
         myName.textContent = 'Sebastian';
     } else {
-        myName.textContent = '';
+        myName.textContent = '____';
     }
 });
-showButton.classList.remove('hide');
+
 showButton.addEventListener('click', () => {
-    animal.classList.remove('hidden');
-    showButton.classList.add('hide');
+    animal.classList.toggle('hidden');
 });
